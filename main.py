c589565bf15933bd808e471dc820576532d14b29
Rndayz <daysraining@163.com>
2024-03-17 11:48:27 +0800

    feat

--- main.py
+++ main.py
@@ -1,134 +1,246 @@
-import os
-from machine import UART
-from machine import Timer
-from machine import I2C,Pin
-import ssd1306 as oled
 import time
-import select
-import bluetooth
-from ssd1306 import SSD1306_I2C   
-#——————gps————————
+import utime
+import struct
+from machine import UART, I2C, Pin
+from ssd1306 import SSD1306_I2C
+import ubluetooth as bt
+from machine import WDT
 
-# SGPGSV语句的基本格式如下:
-# $GPGSV,(1),(2),(3),(4),(5),(6),(7)..(4),(5),(6),()#hh(CR(LF)
-# (1)GSV语句总数。
-# (2)本句GSV的编号。
-# (3)可见卫星的总数(00-12,前面的0也将被传输)
-# (4)卫星编号(01-32，前面的0也将被传输)。
-# (5)卫星仰角(00-90度，前面的0也将被传输)
-# （6）卫星方位角(000-359度，前面的0也将被传输)
+wdt = WDT(timeout=10000)#定义一个看门狗
 
-# SGPRMC,(1),.(2),(3).(4),(5),(6),(7),8),(9),(10),(11),(12)#hb(CR)(LF)
-# (1）UTC时间，hhmmss (时分秒)
-# （2）定位状态，A=有效定位，V=无效定位
-# (3)纬度dmm.mmmmm (度分)
-# （4)纬度半球N(北半球)或S(南半球)
-# (5) 经度dldmm.mmmmm(度分)
-# (6)经度半球E(东经)或W(西经)
-# (7)地面速率(000.0-999.9节)
-# (8)地面航向(0000-359.9度，以真北方为参考基准)
-# (9)UTC日期，ddmmyy(日月年）
-# (10)磁偏角(000.0-180.0度，前导位数不足则补0)
-# (11)磁偏角方向，E(东)或W(西)
-# (12)模式指示(A=自主定位，D=差分，E=估算，N=数据无效)
-
-# $GPVTG (地面速度信息，Track Made Good and Ground Speed)
-# SGPVTG语句的基本格式如下:
-# $GPVTG,(1D),T2),M,(3),N,(4).K,(5)"hh(CR)(LF)
-# (1)以真北为参考基准的地面航向(000-359度，前面的0也将被传输)
-# (2)以磁北为参考基准的地面航向(000-359度，前面的0也将被传输)
-# (3)地面速率(000.0-999.9节，前面的0也将被传输)
-# (4)地面速率0000.0-1851.8公里/小时，前面的0也将被传输#
-# (5)模式指示(A=自主定位，D=差分，E=估算，N=数据无效)
-
-
-uart = UART(1,9600,rx=11, tx=12)
-i2c = I2C(scl=Pin(48), sda=Pin(47), freq=10000)
+# 定义 UART 和 OLED
+uart = UART(1, 9600, rx=11, tx=12)
+i2c = I2C(scl=Pin(48), sda=Pin(47), freq=100000)
 oled = SSD1306_I2C(128, 64, i2c)
-#格式：UART(1, baudrate=115201, bits=8, parity=None, stop=1, tx=10, rx=9, rts=-1, cts=-1, txbuf=256, rxbuf=256, timeout=0, timeout_char=0)
-#定义GPS模块收发IO口
-normal_data_GPGSV = ''
-normal_data_GPVTG = ''
-normal_data_GPRMC = ''
-received_data = ''                                              
+
+# 定义GPS data，防止轮询因为没有查到数据报错
 visiblesat = ''
 speed = ''
 status = ''
-#统一定义变量，防止因为没有轮询到相应回复导致变量未定义
-#定义OLED
+
+# Global variable for initial minutes
+initial_minutes = 0
+
+# BLE UUIDs
+__UART_UUID = bt.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+__RX_UUID = bt.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
+__TX_UUID = bt.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
+__UART_SERVICE = (
+    __UART_UUID,
+    (
+        (__TX_UUID, bt.FLAG_NOTIFY,),
+        (__RX_UUID, bt.FLAG_WRITE,),
+    ),
+)
+
+# BLE UART class
+class BLEConst(object):
+    class IRQ(object):
+        IRQ_CENTRAL_CONNECT = const(1)
+        IRQ_CENTRAL_DISCONNECT = const(2)
+        IRQ_GATTS_WRITE = const(3)
+
+class BLEAppearance:
+    Unknown = 0
+    GENERIC_PHONE = 64
+    GENERIC_COMPUTER = 128
+
+class BLEADType:
+    AD_TYPE_FLAGS = 0x01
+    AD_TYPE_16BIT_SERVICE_UUID_COMPLETE = 0x03
+    AD_TYPE_32BIT_SERVICE_UUID_COMPLETE = 0x05
+    AD_TYPE_128BIT_SERVICE_UUID_COMPLETE = 0x07
+    AD_TYPE_COMPLETE_LOCAL_NAME = 0x09
+    AD_TYPE_APPEARANCE = 0x19
+
+class BLETools:
+    @staticmethod
+    def advertising_generic_payload(limited_disc=False, br_edr=False, name=None, services=None, appearance=0):
+        payload = bytearray()
+
+        def _append(adv_type, value):
+            nonlocal payload
+            payload += struct.pack('BB', len(value) + 1, adv_type) + value
+
+        _append(BLEADType.AD_TYPE_FLAGS, struct.pack('B', (0x01 if limited_disc else 0x02) + (0x00 if br_edr else 0x04)))
+
+        if name:
+            _append(BLEADType.AD_TYPE_COMPLETE_LOCAL_NAME, name)
+
+        if services:
+            for uuid in services:
+                b = bytes(uuid)
+                if len(b) == 2:
+                    _append(BLEADType.AD_TYPE_16BIT_SERVICE_UUID_COMPLETE, b)
+                elif len(b) == 4:
+                    _append(BLEADType.AD_TYPE_32BIT_SERVICE_UUID_COMPLETE, b)
+                elif len(b) == 16:
+                    _append(BLEADType.AD_TYPE_128BIT_SERVICE_UUID_COMPLETE, b)
+
+        _append(BLEADType.AD_TYPE_APPEARANCE, struct.pack('<h', appearance))
+
+        return payload
+
+    @staticmethod
+    def advertising_resp_payload(name=None, services=None):
+        payload = bytearray()
+
+        def _append(adv_type, value):
+            nonlocal payload
+            payload += struct.pack('BB', len(value) + 1, adv_type) + value
+
+        if name:
+            _append(BLEADType.AD_TYPE_COMPLETE_LOCAL_NAME, name)
+
+        if services:
+            for uuid in services:
+                b = bytes(uuid)
+                if len(b) == 2:
+                    _append(BLEADType.AD_TYPE_16BIT_SERVICE_UUID_COMPLETE, b)
+                elif len(b) == 4:
+                    _append(BLEADType.AD_TYPE_32BIT_SERVICE_UUID_COMPLETE, b)
+                elif len(b) == 16:
+                    _append(BLEADType.AD_TYPE_128BIT_SERVICE_UUID_COMPLETE, b)
+
+        return payload
+
+    @staticmethod
+    def decode_mac(addr):
+        if isinstance(addr, memoryview):
+            addr = bytes(addr)
+
+        assert isinstance(addr, bytes) and len(addr) == 6, ValueError("mac address value error")
+        return ":".join(['%02X' % byte for byte in addr])
+
+class BLEUART:
+    def __init__(self, ble, rx_callback=None, name="mpy-uart", rxbuf=100):
+        self.__ble = ble
+        self.__rx_cb = rx_callback
+        self.__conn_handle = None
+        self.__write = self.__ble.gatts_write
+        self.__read = self.__ble.gatts_read
+        self.__notify = self.__ble.gatts_notify
+        self.__ble.active(False)
+        print("activating ble...")
+        self.__ble.active(True)
+        print("ble activated")
+        self.__ble.config(rxbuf=rxbuf)
+        self.__ble.irq(self.__irq)
+        self.__register_services()
+        self.__adv_payload = BLETools.advertising_generic_payload(
+            services=(__UART_UUID,),
+            appearance=BLEAppearance.GENERIC_COMPUTER,
+        )
+        self.__resp_payload = BLETools.advertising_resp_payload(
+            name=name
+        )
+        self.__advertise()
+
+    def __register_services(self):
+        (
+            (
+                self.__tx_handle,
+                self.__rx_handle,
+            ),
+        ) = self.__ble.gatts_register_services((__UART_SERVICE,))
+
+    def __advertise(self, interval_us=500000):
+        self.__ble.gap_advertise(None)
+        self.__ble.gap_advertise(interval_us, adv_data=self.__adv_payload, resp_data=self.__resp_payload)
+        print("advertising...")
+
+    def __irq(self, event, data):
+        global initial_minutes
+        if event == BLEConst.IRQ.IRQ_CENTRAL_CONNECT:
+            self.__conn_handle, addr_type, addr, = data
+            print("[{}] connected, handle: {}".format(BLETools.decode_mac(addr), self.__conn_handle))
+            self.__ble.gap_advertise(None)
+        elif event == BLEConst.IRQ.IRQ_CENTRAL_DISCONNECT:
+            self.__conn_handle, _, addr, = data
+            print("[{}] disconnected, handle: {}".format(BLETools.decode_mac(addr), self.__conn_handle))
+            self.__conn_handle = None
+            self.__advertise()
+        elif event == BLEConst.IRQ.IRQ_GATTS_WRITE:
+            conn_handle, value_handle = data
+            if conn_handle == self.__conn_handle and value_handle == self.__rx_handle:
+                if self.__rx_cb:
+                     received_data = self.__read(self.__rx_handle).decode('utf-8')  # 解码为utf-8格式
+                     self.__rx_cb(received_data)
+                     print("Received data:", received_data)
+                     initial_minutes = int(received_data)*60*1000 + utime.ticks_ms()
+
+    def send(self, data):
+        data_bytes = struct.pack('I', data)
+        self.__write(self.__tx_handle, data_bytes)
+
+        if self.__conn_handle is not None:
+            self.__notify(self.__conn_handle, self.__tx_handle, data_bytes)
+
+    def timmme(self):
+        global initial_minutes
+        # 主循环
+        while initial_minutes > 0:
+            # 清除屏幕内容
+            oled.fill(0)
+            oled.text("Satellites: {}".format(visiblesat), 0, 0)
+            oled.text("Speed: {} km/s".format(speed), 0, 10)
+            oled.text("Status: {}".format(status), 0, 20)
+            oled.text("{}mins ".format(round((initial_minutes - utime.ticks_ms())/1000/60),2), 0, 30)
+            oled.show()
+            if uart.any():
+               received_data = uart.readline().decode('utf-8').strip()
+               print(received_data)
+               parse_gps_data(received_data)
+            time.sleep(1)  # 调整休眠时间，根据需要进行修改
+
+def rx_callback(data):
+    global uart_instance  # 使用 global 关键字声明全局变量
+    global initial_minutes
+    uart_instance.initial_minutes = data  # 将接收到的数字赋值给实例变量
+
+# BLE 实例初始化
+ble = bt.BLE()
+uart_instance = BLEUART(ble, rx_callback)
+
+# OLED 显示
+def update_oled():
+    global initial_minutes
+    oled.fill(0)
+    oled.text("Sats: {}".format(visiblesat), 0, 0)
+    oled.text("Speed: {} km/s".format(speed), 0, 10)
+    oled.text("Status: {}".format(status), 0, 20)
+    oled.text("{}mins ".format(round((initial_minutes - utime.ticks_ms())/1000/60),2), 0, 30)
+    oled.show()
+ 
+
+# GPS data parsing function
+def parse_gps_data(received_data):
+    global visiblesat, speed, status
+    if "GPGSV" in received_data:
+        visiblesat = received_data.split(',')[3]
+    elif "GPVTG" in received_data:
+        speed = received_data.split(',')[7]
+    elif "GPRMC" in received_data:
+        data = received_data.split(',')
+        status = data[2]
+        utctime = data[1]
+        print("UTC Time:", utctime)
+    update_oled()
+
+# 主循环
 while True:
-    if uart.any():
-       received_data = uart.readline().decode('utf-8').strip()#阅读一行，除去换行符，转为普通字符串
-       print(received_data)
-       if "GPGSV" in received_data:
-          normal_data_GPGSV = received_data
-          print("可见卫星信息:", normal_data_GPGSV)
-       elif "GPVTG" in received_data:
-          normal_data_GPVTG = received_data
-          print("地面速度信息:", normal_data_GPVTG)
-       elif "GPRMC" in received_data:
-          normal_data_GPRMC = received_data
-          print("推荐定位信息:", normal_data_GPRMC)
-       time.sleep(0.5)   
-       # 开始分割
-       split_data_GPGSV = normal_data_GPGSV.split(',')
-       split_data_GPVTG = normal_data_GPVTG.split(',')
-       split_data_GPRMC = normal_data_GPRMC.split(',')
-       print(split_data_GPGSV,split_data_GPVTG,split_data_GPRMC)
+    try:
+        if uart.any():
+            received_data = uart.readline().decode('utf-8').strip()
+            print(received_data)
+            parse_gps_data(received_data)
+        
+        wdt.feed() # 喂狗
+        uart_instance.timmme()
+        utime.sleep_ms(500)  # 等待500毫秒
+    except UnicodeError:
+        print("UnicodeError")
+
        
-       if len(split_data_GPGSV) >= 2:
-          visiblesat = split_data_GPGSV[3]
-          print("可见卫星数:", visiblesat)
-       if len(split_data_GPVTG) >= 2:
-          speed = split_data_GPVTG[7]
-          print("当前时速", speed ,'km/s')
-       if len(split_data_GPRMC) >= 2:
-          utctime = split_data_GPRMC[1]
-          status = split_data_GPRMC[2]
-          print("当前UTC时间:", utctime)
-          print("定位状态:", status)  
-       time.sleep(1)
-    else : print('unknown:',received_data)
-    time.sleep(1)
-    oled.text("sts: {}".format(visiblesat), 0, 0)
-    oled.text("spd: {}".format(speed), 0, 10)
-    oled.text("stu: {}".format(status), 0, 20)
-    oled.show()                                                 
-    
-
-#OLED_WR_Byte(0xA1,OLED_CMD);//--Set SEG/Column Mapping     0xa0左右反置 0xa1正常
-#OLED_WR_Byte(0xC8,OLED_CMD);//Set COM/Row Scan Direction   0xc0上下反置 0xc8正常
-#可能用于上下翻转显示
-# 
-# 
-# display.poweroff（）#关闭显示器电源，像素保留在内存中
-# display.poweon（）#打开显示器电源，重新绘制像素
-# display.contation（0）#dim
- # display.contation（255）#明亮
-# display.inverse（1）#显示反转
-# display.inverse（0）#显示正常
-# display.rotate（True）#旋转180度
-# display.rate（False）#旋转0度
-# display.show（）#将FrameBuffer的内容写入显示内存
-# 子类化 帧缓冲区提供对图形基元的支持：
-# # 
-# display.fill（0）#用颜色填充整个屏幕=0
-# display.pixel（0,10）#在x=0，y=10处获取像素
-# display.pixel（0，10，1）#将x=0，y=10处的像素设置为color=1
-# display.hline（0，8，4，1）#绘制水平线x=0，y=8，宽度=4，颜色=1
-# display.vline（0，8，4，1）#绘制垂直线x=0，y=8，高度=4，颜色=1
-# display.line（0，0，127，63，1）#画一条从0,0到127,63的线
-# display.rect（10，10，107，43，1）#绘制矩形轮廓10，10到107，43
-# display.fill_rect（10，10，107，43，1）#绘制一个实心矩形10,10到107,43，color=1
-# display.text（“Hello World”，0，0，1）#在x=0，y=0，colour=1处绘制一些文本
-# display.scroll（20，0）#向右滚动20个像素#
-# #在给定坐标的当前帧缓冲区的顶部绘制另一个帧缓冲区
-# # import framebuf
-# fbuf = framebuf.FrameBuffer(bytearray(8 * 8 * 1), 8, 8, framebuf.MONO_VLSB)
-# fbuf.line(0, 0, 7, 7, 1)
-# display.blit(fbuf, 10, 10, 0)           # draw on top at x=10, y=10, key=0
-# display.show()
-
-#——bluetooth——
-#esp32的蓝牙地址为 34:85:18:AC:70:C6
+   
 
